9053746ee0d8b979c92eeb062100b001d8637c28
rarmiger <rarmiger@gmail.com>
2016-01-24 17:30:42 -0500

    remove pygame for timing
    Former-commit-id: 8f094677887b67dbcd6110fd7e0eecd1920fa6ef

--- python/Plant.py
+++ python/Plant.py
@@ -12,22 +12,32 @@ Created on Sat Jan 23 20:38:47 2016
 import math
 
 VERBOSE = 1;
+DEBUG = 1;
    
 class Plant(object):
 
-    def __init__(self):
+    def __init__(self,dt):
         NUM_JOINTS = 27;
         self.position = [0.0]*NUM_JOINTS
         self.velocity = [0.0]*NUM_JOINTS
         self.limit = [45.0 * math.pi / 180.0]*NUM_JOINTS
+        self.dt = dt
 
-        self.velocity[4] = 30.0 * math.pi / 180.0
+        if DEBUG:
+            # debug, set a joint to move
+            self.velocity[4] = 30.0 * math.pi / 180.0
 
-    def update(self,dt):
+    def update(self):
+        # perform time integration based on elapsed time, dt
         
         for i,item in enumerate(self.position):  
-            self.position[i] = self.position[i] + self.velocity[i]*dt/1000;
+            self.position[i] = self.position[i] + self.velocity[i]*self.dt;
         
+            # TODO: only works symmetrically
             if abs(self.position[i]) > self.limit[i] :
-                self.velocity[i] = -self.velocity[i]
+                # Saturate at either high or low limit
                 self.position[i] = math.copysign(self.limit[i],self.position[i])
+                
+                if DEBUG:
+                    # Debug only 
+                    self.velocity[i] = -self.velocity[i]
--- python/sample_main.py
+++ python/sample_main.py
@@ -2,57 +2,48 @@
 #
 # Created 1/23/2016 Armiger
 
-from pygame import time
 import math
-import time as tm
+import time
 from MyoUdp import MyoUdp
 from Plant import Plant
 from UnityUdp import UnityUdp
+import sys
 
 VERBOSE = 1;
 
+dt = 0.02  # seconds per loop.  50Hz update
+
 # Create data objects    
-myPlant = Plant()
+hPlant = Plant(dt)
 hSink = UnityUdp("192.168.1.24")
 hMyo = MyoUdp()#("192.168.1.3")
 
-# represents the overhead associated with the main loop compared to the fixed rate output (50Hz)
-numUpdates = 0
 try: 
     # setup main loop control
-    time_elapsed_since_last_action = 0
-    clock = time.Clock()
     print("Running...")
-    while True: # main loop
-        numUpdates = numUpdates + 1
-        
-        # the following method returns the time since its last call in milliseconds
-        # it is good practice to store it in a variable called 'dt'
-        dt = clock.tick() 
+    sys.stdout.flush()
     
-        #tm.sleep(0.001)
-        myPlant.update(dt)
+    while True: # main loop
+        timeBegin = time.time()
+    
+        hPlant.update()
         
+        # perform joint update
+        vals = hMyo.getAngles()
+        hPlant.position[3] = vals[1] + math.pi/2
         
-        time_elapsed_since_last_action += dt
-        # dt is measured in milliseconds, therefore 20 ms = 0.02 seconds = 50Hz
-        if time_elapsed_since_last_action >= 20:
+        # transmit output
+        hSink.sendJointAngles(hPlant.position)
+        
+        if VERBOSE:
+            print(("%8.4f" % hPlant.position[3], "%8.4f" % hPlant.position[4]))
 
-            # perform joint update
-            vals = hMyo.getAngles()
-            myPlant.position[3] = vals[1] + math.pi/2
-            
-            # transmit output
-            hSink.sendJointAngles(myPlant.position)
-            
-            if VERBOSE:
-                print(("%4d" % numUpdates, "%8.4f" % myPlant.position[3], "%8.4f" % myPlant.position[4] ))
-
-            # reset timing counter vars
-            numUpdates = 0;
-            time_elapsed_since_last_action = 0 # reset it to 0 so you can count again
+        timeEnd = time.time()
+        timeElapsed = timeEnd - timeBegin
+        time.sleep(dt-timeElapsed)
             
 finally:
     print(hMyo.emg_buffer)
+    print("Last timeElapsed was: ", timeElapsed)
     hSink.close()
     hMyo.close()

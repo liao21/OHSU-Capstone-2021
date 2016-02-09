e3027af0789dcfe4246dbaa066dc35a725e1d303
rarmiger <rarmiger@gmail.com>
2016-02-09 12:10:00 -0500

    comments
    Former-commit-id: 65eb6e1dafc1d397a3db3ff2507258b2f0a291e9

--- python/sample_main.py
+++ python/sample_main.py
@@ -14,6 +14,7 @@ import numpy as np
 from MyoUdp import MyoUdp
 from Plant import Plant
 from UnityUdp import UnityUdp
+from TrainingUdp import TrainingUdp
 from feature_extract import feature_extract
 
 VERBOSE = 1
@@ -21,19 +22,34 @@ VERBOSE = 1
 dt = 0.02  # seconds per loop.  50Hz update
 
 # Create data objects
-hPlant = Plant(dt)
-hSink = UnityUdp("192.168.1.24")
+
+# Singal Source get external bio-signal data
 hMyo = MyoUdp()#("192.168.1.3")
 
+# Training Data holds data labels 
+hTrain = TrainingUdp()
+
+# Plant maintains current limb state (positions) during velocity control
+hPlant = Plant(dt)
+
+# Sink is output to ouside world (in this case to VIE)
+hSink = UnityUdp() #("192.168.1.24")
+
+# Classifier parameters
 W = np.genfromtxt("weights.txt", dtype=None)
 C = np.genfromtxt("centers.txt", dtype=None)
+# Classifier class names 
 with open("classes.txt") as f:
     classNames = f.read().splitlines()
 
+trainCurrentClass = []
 
+#create filefor dumping training data
+file=open('tmp.dat','w')
+     
 # Iteration counter
-cycleMax = 1000
-cycleCnt = 0
+cycleMax = 1000  # Max iterations (0 for infinite)
+cycleCnt = 0  # Internal Counter
 timeElapsed = -1
 try:
     # setup main loop control
@@ -42,18 +58,26 @@ try:
 
     while True: # main loop
 
+        # LOOP CONTROL        
         # Terminate after certain muber of steps
         cycleCnt = cycleCnt + 1
         if (cycleMax > 0) and cycleCnt > cycleMax:
             break
+        # LOOP CONTROL        
 
+        # LOOP TIMING (to achieve fixed rate)
         timeBegin = time.time()
 
-        f = feature_extract(hMyo.getData()*0.01)
 
+        # Get data and extract features
+        # feature vector should be [1,nChan*nFeat]
+        # data ordering is as follows
+        # [ch1f1, ch1f2, ch1f3, ch1f4, ch2f1, ch2f2, ch2f3, ch2f4, ... chNf4]
+        f = feature_extract(hMyo.getData()*0.01)
+        
         # Classify
         # features[1,nChan*nFeat] * Wg[nChan*numFeat,nClasses] + Cg[1,nClasses]
-        v = np.dot(f.T.reshape(1, 32), W) + C
+        v = np.dot(f, W) + C
         classNum = v.argmax()
 
         # Move joints using classifier
@@ -74,12 +98,26 @@ try:
         # transmit output
         hSink.sendJointAngles(hPlant.position)
 
+
+        # Training Process begin logging
+        cName = str(hTrain.class_name)
+        if cName:
+            print(cName)
+            #txt = ','.join(map(str, f.tolist()))
+            f.tofile(file)
+            #file.write(' %d %s\n'%(hTrain.class_id, cName))
+        
+        # Training Process end
+
+
+        # DEBUG output display
         if VERBOSE:
             #print(f[:1,:])
             print(("%8.4f" % hPlant.position[3], \
             "%8.4f" % hPlant.position[4]), \
             'Class: %s' % classNames[classNum])
 
+        # LOOP TIMING (to achieve fixed rate)
         timeEnd = time.time()
         timeElapsed = timeEnd - timeBegin
         if dt > timeElapsed:
@@ -92,3 +130,5 @@ finally:
     print("Last timeElapsed was: ", timeElapsed)
     hSink.close()
     hMyo.close()
+    hTrain.close()
+    file.close()     #close file  

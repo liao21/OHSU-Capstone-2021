3fbe2c352c8864bee09900a6d8bd8c46a8584365
rarmiger <rarmiger@gmail.com>
2016-02-18 17:36:25 -0500

    added xml parsing for RunMpl
    Former-commit-id: 61867803367fb75d139029cb650111845ab695b3

--- Utilities/RunMpl.m
+++ Utilities/RunMpl.m
@@ -1,34 +1,13 @@
 function [ obj ] = RunMpl
 
-% Set patient parameter set
-cd('C:\MiniVIE_Users\JHMI_TH01_L')
-UserConfig.getInstance('JHMI_TH01_L_user_config.xml');
-
-p.guiName = 'MiniVIE-NFU-MPL';
-p.filePrefix = UserConfig.getUserConfigVar('userFilePrefix','NEW_USER_');
-strNfuIp = UserConfig.getUserConfigVar('mplNfuIp','192.168.1.111');
-
-UiTools.save_temp_file('defaultFilePrefix',p.filePrefix)
-
+% setup console logging (to current directory)
 diaryFile = [datestr(now,'yyyymmdd_HHMMSS') '_diary.txt'];
 diary(diaryFile);
 
-% define input source
-p.hSource = Inputs.MyoUdp.getInstance();
-
-p.Scenario = MPL.MplNfuScenario;
-p.Scenario.JoystickId = 0;
-
-% Haptics need to be configured here, and in 
-p.Scenario.EnableFeedback = 1;
-p.Scenario.EnableImpedance = 0;
-% p.Scenario.TactorIds = [5 6 7];
-p.Scenario.TactorIds = [3 4];  % JH_TH_01
-
 % perform windows ping prior to further initialization
-wait_for_ping_response(strNfuIp);
+wait_for_ping_response(UserConfig.getUserConfigVar('mplNfuIp','192.168.1.111'));
 
-obj = foo(p);
+obj = run();
 
 end
 
@@ -50,96 +29,101 @@ while ~pingOk
 end % while
 end % wait_for_ping_response
 
-function obj = foo(p)
+function obj = run()
+% main run function
+
 obj = MiniVIE;
-set(obj.hg.Figure,'Name',p.guiName);
-obj.FilePrefix = p.filePrefix;
+set(obj.hg.Figure,'Name','MiniVIE-NFU-MPL');
+obj.FilePrefix = UserConfig.getUserConfigVar('userFilePrefix','NEW_USER_');
+UiTools.save_temp_file('defaultFilePrefix',obj.FilePrefix)
 
 %% Inputs
-h = p.hSource;
-h.NumSamples = 4000;
-h.initialize();
+obj.SignalSource = eval(UserConfig.getUserConfigVar('SignalSource','Inputs.SignalSimulator()'));
+obj.SignalSource.NumSamples = 4000;
+obj.SignalSource.initialize();
 
 % Enable buttons
 set(obj.hg.SignalSourceButtons(:),'Enable','on');
-set(obj.hg.popups(1),'Value',10);    % select the NFU input source
+% TODO lookup correct input number
+%set(obj.hg.popups(1),'Value',10);    % select the NFU input source
+set(obj.hg.popups(1),'Value',6);    % select the NFU input source
 
 % Setup filters and remaining properties
-% obj.println('Adding Filters',1);
-% Fs = h.SampleFrequency;
-% h.addfilter(Inputs.HighPass(20,3,Fs));
-% % h.addfilter(Inputs.RemoveOffset(10));
+cellFilterCreators = eval(UserConfig.getUserConfigVar('SignalFilters','{}'));
+for i = 1:length(cellFilterCreators)
+    obj.println(sprintf('Creating Filter: %s',cellFilterCreators{i}),1);
+    hFilter = eval(cellFilterCreators{i});
+    obj.SignalSource.addfilter(hFilter);
+end
+
 % % h.addfilter(Inputs.Notch([120 240 360],5,1,Fs));
 % % h.addfilter(Inputs.Notch([120 240 360],64,1,1000));
-% 
-% Fs = h.SampleFrequency;                     % 1000 Hz
-% h.addfilter(Inputs.HighPass(20,3,Fs));      % 20Hz 3rd order butter
-% h.addfilter(Inputs.MinLimitFilter(0.2));    % min limit
-% h.addfilter(Inputs.ConstraintFilter(-5,5)); % range limit
-
-obj.SignalSource = h;
 
 % f = GUIs.guiSignalViewer(h);
 % uiwait(f.hg.Figure);
 
 %% Signal Analysis
-h = SignalAnalysis.Lda();
+obj.SignalClassifier = SignalAnalysis.Lda();
 
 % Enable Buttons
 set(obj.hg.SignalAnalysisButtons(:),'Enable','on');
 set(obj.hg.popups(2),'Value',2);
 
-h.NumMajorityVotes = 3;
+obj.SignalClassifier.NumMajorityVotes = 3;
 
-NumSamplesPerWindow = 250;
+NumSamplesPerWindow = 150;
 fprintf('Setting Window Size to: %d\n',NumSamplesPerWindow);
-h.NumSamplesPerWindow = NumSamplesPerWindow;
+obj.SignalClassifier.NumSamplesPerWindow = NumSamplesPerWindow;
 
 %obj.TrainingData = PatternRecognition.TrainingData();
 obj.TrainingData = TrainingDataAnalysis();
 if ~obj.TrainingData.loadTrainingData([])
     % Initialize with defaults
-    obj.TrainingData.initialize(obj.SignalSource.NumChannels,h.NumSamplesPerWindow);
+    obj.TrainingData.initialize(obj.SignalSource.NumChannels,NumSamplesPerWindow);
 end
 
 set(obj.hg.TrainingButtons(:),'Enable','on');
 
 % Initialize Classifier with data object
-h.initialize(obj.TrainingData);
+obj.SignalClassifier.initialize(obj.TrainingData);
 
 % TODO: Note signals only updated on classifier
 % creation
-% defaultChannels = 1:8;
-defaultChannels = GUIs.guiChannelSelect.getLastChannels();
+defaultChannels = 1:16;
+% defaultChannels = GUIs.guiChannelSelect.getLastChannels();
 if isempty(defaultChannels)
     msg = 'No channels are active.  Enable channels in Signal Viewer';
     errordlg(msg);
     error(msg);
 end
-h.setActiveChannels(defaultChannels);
+obj.SignalClassifier.setActiveChannels(defaultChannels);
 
 classNames = GUIs.guiClassifierChannels.getSavedDefaults();
 if (isempty(classNames))
     classNames = GUIs.guiClassifierChannels.getDefaultNames;
 end
-h.setClassNames(classNames);
+obj.SignalClassifier.setClassNames(classNames);
 
-obj.SignalClassifier = h;
 obj.SignalClassifier.train();
 
 %% Setup Presentation
-h = p.Scenario;
-h.initialize(obj.SignalSource,obj.SignalClassifier,obj.TrainingData);
-h.Verbose = 0;
-h.update();
+obj.Presentation = eval(UserConfig.getUserConfigVar('Scenario','Scenarios.OnlineRetrainer'));
+obj.Presentation.JoystickId = 0;
 
-start(h.Timer);
+% Haptics need to be configured here, and in 
+obj.Presentation.EnableFeedback = 0;
+obj.Presentation.EnableImpedance = 0;
+obj.Presentation.TactorIds = [3 4];
+
+obj.Presentation.initialize(obj.SignalSource,obj.SignalClassifier,obj.TrainingData);
+obj.Presentation.Verbose = 0;
+obj.Presentation.update();
+
+obj.Presentation.start();
 obj.println('Presentation setup complete',1);
 
 % Enable buttons
 set(obj.hg.PresentationButtons(:),'Enable','on');
 set(obj.hg.popups(5),'Value',6);
-
-obj.Presentation = h;
 
 end

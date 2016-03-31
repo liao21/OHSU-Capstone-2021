c93809d779a915df221522e65930965846fefffe
rarmiger <rarmiger@gmail.com>
2016-03-31 16:38:55 -0400

    added more extensive ping messages
    Former-commit-id: 25f4537b9f68000f0c2d539f7d79366d0fa5643b

--- Utilities/RunMpl.m
+++ Utilities/RunMpl.m
@@ -5,27 +5,51 @@ diaryFile = [datestr(now,'yyyymmdd_HHMMSS') '_diary.txt'];
 diary(diaryFile);
 
 % perform windows ping prior to further initialization
-wait_for_ping_response(UserConfig.getUserConfigVar('mplNfuIp','192.168.1.111'));
+wait_for_ping_response();
 
 obj = run();
 
 end
 
-function wait_for_ping_response(ip)
+function wait_for_ping_response()
 % Function blocks until a successful ping from the specified device
 
-fprintf('Trying to connect to %s...\n',ip);
+
+ipRouter = UserConfig.getUserConfigVar('mplRouterIp','192.168.1.1');
+ipArm = UserConfig.getUserConfigVar('mplNfuIp','192.168.1.111');
+
+fprintf('Trying to connect to Router @ %s and MPL @ %s ...\n',ipRouter,ipArm);
+
 pingOk = false;
+
 while ~pingOk
-    strPing = sprintf('!ping %s -n 1',ip);
-    strSuccess = sprintf('Reply from %s: bytes=32',ip);
+    % ping router
+    strPing = sprintf('!ping %s -n 1',ipRouter);
+    strSuccess = sprintf('Reply from %s: bytes=32',ipRouter);
     response = evalc(strPing);
     foundId = strfind(response,strSuccess);
-    pingOk = ~isempty(foundId);
-    
-    if ~pingOk
-        fprintf('Ping Failed\n')
+    routerOk = ~isempty(foundId);
+    if routerOk
+        routerStatus = 'OK';
+    else
+        routerStatus = 'FAILED';
     end
+    
+    strPing = sprintf('!ping %s -n 1',ipArm);
+    strSuccess = sprintf('Reply from %s: bytes=32',ipArm);
+    response = evalc(strPing);
+    foundId = strfind(response,strSuccess);
+    mplOk = ~isempty(foundId);
+    if mplOk
+        mplStatus = 'OK';
+    else
+        mplStatus = 'FAILED';
+    end
+    
+    fprintf('Router Ping @ %s = %s; MPL Ping @ %s = %s\n',ipRouter,routerStatus,ipArm,mplStatus);
+    
+    pingOk = routerOk & mplOk;
+    
 end % while
 end % wait_for_ping_response
 

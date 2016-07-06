e34a6befafe5580067431c6aac4d4b27281055cc
David Samson <David.Samson@jhuapl.edu>
2016-07-06 13:10:25 -0400

    Myo UDP controller demo now has simplified or complex control method selection. Also updated complex control switch to use class names
    Former-commit-id: 067abfb3ecbf465cc38e610f9c7201e3f048877c
    Former-commit-id: 234c4ee97980f7bc262538a7f936c153c7be5019

--- Demos/Demo_Myo_UDP_Game_Controller.m
+++ Demos/Demo_Myo_UDP_Game_Controller.m
@@ -2,8 +2,7 @@
 % By David Samson
 % July 1, 2016
 
-% Send controller data via UDP
-% Matlab packs data 
+% Send controller data via UDP based on Myo input 
 
 a = PnetClass(3452,5005,'192.168.56.101');
 a.initialize;
@@ -12,10 +11,18 @@ joystickType = 'Default'; % SNES equivalent. May use 2 axes and up to 8 buttons
 a.putData([255 uint8('T') uint8(joystickType)]) %send joystick selection to reciever
 
 
-% Load training data file
+% Create arrays to hold the buttons and the control axes
+btns = [0 0 0 0 0 0 0 0];
+axes = [0 0];
+
+
+% Determine type of control scheme by Myo band
+simpleControl = true; % Myo controls using raw emg magnitudes 
+% Try to load training data file
 hData = PatternRecognition.TrainingData();
-hData.loadTrainingData();
-%hData.loadTrainingData('C:\Users\samsoda1\Desktop\EMG Biointerfacing Lab\VIE Introduction Materials\user.trainingData');
+if (hData.loadTrainingData())
+    simpleControl = false; % Myo controls using LDA classifier
+end
 
 
 % Create EMG Myo Interface Object
@@ -27,6 +34,7 @@ hLda = SignalAnalysis.Lda;
 hLda.initialize(hData);
 hLda.train();
 hLda.computeError();
+classes = hLda.getClassNames;
 
 % Store the 6 most recent states of the predicted hand motion.
 % Current motion is determined by the mode of these states
@@ -34,10 +42,16 @@ hLda.computeError();
 state = [9 9 9 9 9 9];
 
 
-% Create arrays to hold the buttons and the control axes
-btns = [0 0 0 0];
-axes = [0 0];
 
+% Array specifying which Myo channels to use for simple control model
+mChannels = [4,7];
+
+% Amount to scale the difference of the emg signals by
+% higher increases joystick range, but decreases control precision
+mScale = 100000; 
+
+% Store 10 most recent simple control states to smooth simple control output
+avgABS = zeros(1,10);
 %%
 
 % May want to tweak which classes cause what button presses/axis movement
@@ -50,57 +64,71 @@ while StartStopForm
     % Get the appropriate number of EMG samples for the 8 myo channels
     emgData = hMyo.getData(hLda.NumSamplesPerWindow,1:8);
     
-    % Extract features and classify
-    features2D = hLda.extractfeatures(emgData);
-    [classDecision, voteDecision] = hLda.classify(reshape(features2D',[],1));
     
-    
-    
-    % Push current class decision into state buffer
-    state(2:end) = state(1:end-1);
-    state(1) = classDecision;
-    state;
-    classDecision = mode(state);
-    
-    
-    % Perform controller states based on class decision mode
-    % setup for left/right input and one button for jumping
-    switch(classDecision)
-        case 1 % Wrist rotate in
-            fprintf('wrist rotate in\n');
-            axes(1) = 32767; %joystick X = right
-        case 2 % Wrist rotate out
-            fprintf('wrist rotate out\n');
-            axes(1) = -32767; %joystick X = left
-        case 3 % Wrist flex in
-            fprintf('wrist flex in\n');
-            axes(1) = 32767;  %joystick X = right
-        case 4 % Wrist extend out
-            fprintf('wrist extend out\n');
-            axes(1) = -32767;  %joystick X = left
-        case 5 % Cylindrical grasp
-            fprintf('cylindrical grasp\n');
-            btns(1) = 1;   %press jump button
-        case 6 % Tip grasp
-            fprintf('tip grasp\n');
-            btns(1) = 1;   %press jump button
-        case 7 % Lateral grasp
-            fprintf('lateral grasp\n');
-            btns(1) = 1;   %press jump button
-        case 8 % Hand open
-            fprintf('hand open\n');
-            btns(1) = 1;   %press jump button
-        case 9 % No movement
-            fprintf('no movement\n');
-            btns = [0 0 0 0]; %reset buttons
-            axes = [0 0];  %reset axes
-        case 10 % wrist abduction
-            fprintf('wrist abduction\n');
-            axes(2) = -32767;   %joystick Y = Up
-        case 11 %wrist adduction
-            fprintf('wrist adduction\n');
-            axes(2) = 32767;    %joystick Y = Down
-            
+    if (simpleControl)
+        % Do simple control model
+        % To use, wear Myo band with logo facing outward, and notification
+        % LED pointing downward on forearm. Only control input provided in
+        % this mode is simple left and right joystick input. No buttons.
+        avgABS(2:end) = avgABS(1:end-1);
+        avgABS(1) = diff(abs(emgData(1,mChannels)))*mScale;
+        axes(1) = int16(mean(avgABS))
+    else
+        % Complex control model
+        % Generate button presses and joystick positions for current data
+        % classification of Myo emg signals according to LDA classifier
+        % Extract features and classify
+        features2D = hLda.extractfeatures(emgData);
+        [classDecision, voteDecision] = hLda.classify(reshape(features2D',[],1));
+
+
+
+        % Push current class decision into state buffer
+        state(2:end) = state(1:end-1);
+        state(1) = classDecision;
+        state;
+        classDecision = mode(state);
+
+
+        % Perform controller states based on class decision mode
+        % setup for left/right input and one button for jumping
+        switch(char(classes(classDecision)))
+            case 'Wrist Rotate In'
+                fprintf('wrist rotate in\n');
+                axes(1) = 32767; %joystick X = right
+            case 'Wrist Rotate Out'
+                fprintf('wrist rotate out\n');
+                axes(1) = -32767; %joystick X = left
+            case 'Wrist Flex In'
+                fprintf('wrist flex in\n');
+                axes(1) = 32767;  %joystick X = right
+            case 'Wrist Extend Out'
+                fprintf('wrist extend out\n');
+                axes(1) = -32767;  %joystick X = left
+            case 'Cylindrical Grasp'
+                fprintf('cylindrical grasp\n');
+                btns(1) = 1;   %press jump button
+            case 'Tip Grasp'
+                fprintf('tip grasp\n');
+                btns(1) = 1;   %press jump button
+            case 'Lateral Grasp'
+                fprintf('lateral grasp\n');
+                btns(1) = 1;   %press jump button
+            case 'Hand Open'
+                fprintf('hand open\n');
+                btns(1) = 1;   %press jump button
+            case 'No Movement'
+                fprintf('no movement\n');
+                btns = [0 0 0 0]; %reset buttons
+                axes = [0 0];  %reset axes
+            case 'Wrist Abduction'
+                fprintf('wrist abduction\n');
+                axes(2) = -32767;   %joystick Y = Up
+            case 'Wrist Adduction'
+                fprintf('wrist adduction\n');
+                axes(2) = 32767;    %joystick Y = Down
+
+        end
     end
     
     %format buttons and axes for sending over UDP
@@ -111,8 +139,7 @@ while StartStopForm
     
     msg = uint8([length(btns) btn0 length(axes) axis0]);
     a.putData(msg); 
-    
-    drawnow; 
+ 
 end
 
 %a.putData([255 uint8('Q')]) % interrupt code to quit running UDP reciever

ca74b09d459886f4d9137caeb249c383abf8b8ff
Maddie Franke <maddiemfranke@gmail.com>
2016-08-11 17:48:23 +0000

    Servo_Client.py edited online with Bitbucket

--- python/TactorFiles/Servo_Client.py
+++ python/TactorFiles/Servo_Client.py
@@ -3,9 +3,13 @@ import sys
 import Run_Servo
 import time
 
+# This file is creates a UDP client and receives an array of torque values from the UDP run from the MPL.
+# Revisions: 
+# 11-August-2016 Franke: Initial revision
+
 udpSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 clientAddress = ('10.113.64.96', 12001)
-udpSocket.bind(clientAddress)
+udpSocket.bind(clientAddress) # binds to UDP socket 12001
 serverAddress = ('10.113.65.152', 12001)
 
 while (True):
@@ -13,7 +17,7 @@ while (True):
         print >>sys.stdout, 'Waiting to receive'
         data, server = udpSocket.recvfrom(12001)
         print >>sys.stdout, data
-        newData = data.split(" ")
+        newData = data.split(" ") # splits the array into values after each space character
         #newDataOne = newData[0::2]
         #newDataTwo = newData[1::2]
         #Run_Servo.setAngles(newDataOne, newDataTwo)

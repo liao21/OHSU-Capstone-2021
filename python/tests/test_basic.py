fe3794e1e3a59c532b32094e747e39e863e44759
rarmiger <rarmiger@gmail.com>
2016-10-15 00:08:41 -0400

    updated folder structure

--- python/tests/test_basic.py
+++ python/tests/test_basic.py
@@ -7,26 +7,26 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
-os.chdir('..')  # change directory so xml files can be found as expected
+sys.path.insert(0, os.path.abspath('../minivie'))
+os.chdir('../minivie')  # change directory so xml files can be found as expected
 
-import RocTableClass
+from Controls import Plant
+Plant.main()
+
+from MPL import RocTableClass
 RocTableClass.main()
 
-import UserConfigXml
+from Utilities import UserConfigXml
 UserConfigXml.main()
 
-import NfuUdp
+from MPL import NfuUdp
 NfuUdp.main()
 # generates warning for too long parameter name
 nfu = NfuUdp.NfuUdp()
 nfu.msgUpdateParam('-' *160, 0.0)
 
-import OpenNfuMain
+from Scenarios import OpenNfuMain
 OpenNfuMain.main()
-
-#import Plant
-#Plant.main()
 
 #import sample_main
 #sample_main.main()

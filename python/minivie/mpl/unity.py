da22ec594248b5da267acd3d351d21067f6494df
rarmiger <rarmiger@gmail.com>
2016-12-02 18:27:57 -0500

    added percept processing

--- python/minivie/mpl/unity.py
+++ python/minivie/mpl/unity.py
@@ -43,36 +43,66 @@ Example:
 
 TODO: Presently this is only one-way communication.  Receive sensor data
 
+Notes:
+    for debug use:
+    import logging
+    logging.basicConfig(level=logging.DEBUG)
 
 Created on Sat Jan 23 20:36:50 2016
 
 @author: R. Armiger
 """
-#TODO: Presently this is only one-way communication.  Receive sensor data
 
 import socket
-import binascii
 import struct
 import logging
+import numpy as np
 
 
 class UnityUdp(object):
-    def __init__(self, ip="127.0.0.1", port=25000):
-        self.UDP_IP = ip
-        self.UDP_PORT = port
+    """
+        % Left
+        obj.MplCmdPort = 25100;
+        obj.MplLocalPort = 25101;
+        obj.MplAddress = '127.0.0.1';
+        % Right
+        obj.MplCmdPort = 25000;
+        obj.MplLocalPort = 25001;
+        obj.MplAddress = '127.0.0.1';
 
-        logging.info("UnityUdp target IP: {}".format(self.UDP_IP))
-        logging.info("UnityUdp target port: {}".format(self.UDP_PORT))
-
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
+    """
+    def __init__(self, remote_host="127.0.0.1", remote_port=25000, local_port=25001):
+        self.udp = {'RemoteHost': remote_host, 'RemotePort': remote_port, 'LocalPort': local_port}
+        self.sock = None
+        self.is_connected = False
+        self.connect()
 
     def connect(self):
-        pass
+        logging.info("UnityUdp local port: {}".format(self.udp['LocalPort']))
+        logging.info("UnityUdp remote port: {}:{}".format(self.udp['RemoteHost'], self.udp['RemotePort']))
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind(('127.0.0.1', self.udp['LocalPort']))
+        self.sock.settimeout(3.0)
+        self.is_connected = True
 
     def get_voltage(self):
         return 'vMPL'        
         
     def send_joint_angles(self, values):
+        """
+
+        send_joint_angles
+
+        encode and transmit MPL joint angles to unity.
+
+        :param values:
+         Array of joint angles in radians.  Ordering is specified in mpl.JointEnum
+         values can either be the 7 arm values, or 27 arm and hand values
+
+        :return:
+         None
+
+        """
 
         if len(values) == 27:
             pass
@@ -84,12 +114,63 @@ class UnityUdp(object):
             return
 
         # Send data
-        logging.debug('Joint Command:')
-        logging.debug(["{0:0.2f}".format(i) for i in values[0:27]])
+        logging.debug('Joint Command:' + np.array2string(np.array(values), precision=2, separator=','))
         packer = struct.Struct('27f')
         packed_data = packer.pack(*values)
-        self.sock.sendto(packed_data, (self.UDP_IP, self.UDP_PORT))
+        self.sock.sendto(packed_data, (self.udp['RemoteHost'], self.udp['RemotePort']))
+
+    def get_percepts(self):
+        """
+        Get sensor data from vMPL (returns tuple)
+
+        :return:
+
+            joint_data - [27x3] matrix of position velocity and torque for each joint
+            contact_data - Contact pad data
+            segment_data - FTSN Data (old/new)
+
+        """
+        joint_data = None
+        contact_data = None
+        segment_data = None
+
+        # get data from socket, with timeout
+        try:
+            # receive call will error if socket closed on exit
+            data, address = self.sock.recvfrom(1024)  # blocks until timeout
+        except socket.timeout:
+            msg = "Percept Read Timeout"
+            logging.warning(msg)
+            return joint_data, contact_data, segment_data
+        except socket.error:
+            msg = "Error reading data. Socket closed while receiving"
+            logging.warning(msg)
+            return joint_data, contact_data, segment_data
+
+        # Convert raw bytes
+
+        # Upper Arm Joints = 7 Joints * 3 values per joint * 4 bytes per value = 84 bytes)... bytes 0-84
+        # Fingers and Thumb = 20 Joints * 3 values per joint * 4 bytes per value = 240 bytes)... bytes 85-324
+        if len(data) >= 324:
+            joint_data = np.frombuffer(data[0:324], np.float32, 27 * 3)
+
+        # ContactPerceptsType   74 (37 values (2 bytes each), so (2x37) enum for each of potential contact sensors)
+        # FtsnForcePerceptsType 60 (3-axis x 32-bit values (3 x 4 bytes) for each of 5 fingers, so (3x4x5 = 60))
+        # FtsnAccelPerceptsType 60 (3-axis x 32-bit values (3 x 4 bytes) for each of 5 fingers, so (3x4x5 = 60))
+        # FtsnTempPerceptsType  20 (1 x 32-bit value (1 x 4 bytes) for each of 5 fingers, so (1x4x5 = 20))
+        if len(data) >= 398:
+            contact_data = np.frombuffer(data[324:398], np.short, 37)
+
+        # Segment forces (OLD) = 5 sensors * 3 axes/values per sensor * 4 bytes per value = 60 bytes)... bytes 399-458
+        # Segment forces (NEW) = 5 sensors * 14 pads/values per sensor * 4 bytes per value = 280 bytes)... bytes 399-678
+        if len(data) >= 678:
+            segment_data = np.frombuffer(data[398:678], np.float32, 70)
+        elif len(data) >= 458:
+            segment_data = np.frombuffer(data[398:458], np.float32, 15)
+
+        return joint_data, contact_data, segment_data
 
     def close(self):
         self.sock.close()
-        logging.info("Closing UnityUdp Socket IP={} Port={}".format(self.UDP_IP, self.UDP_PORT))
+        logging.info("Closing Local UnityUdp Socket Port={}".format(self.udp['LocalPort']))
+        self.is_connected = False

00b4449406f06554e521efbfbda18e822567ca25
rarmiger <rarmiger@gmail.com>
2016-12-23 11:36:36 -0500

    updated torque parsing and warning message for mpltest

--- Utilities/DiaryParse.m
+++ Utilities/DiaryParse.m
@@ -1,9 +1,11 @@
 classdef DiaryParse
     methods (Static = true)
-        function T = getTorque
+        function T = getTorque(p)
             % DiaryParse.getTorque
             
-            p = 'C:\tmp';
+            if nargin < 1
+                p = 'C:\tmp';
+            end
             s = dir(fullfile(p,'*diary.txt'));
             T = [];
             for i = 1:length(s)
@@ -15,10 +17,33 @@ classdef DiaryParse
                 tag = 'Sensor Data--HR:';
                 dataLines = C{1}(strncmp(tag,C{1},length(tag)));
                 dataVals = cellfun(@(s)str2double(s(17:26)),dataLines);
+                
+                % remove outliers
+                id = dataVals > 200;
+                dataVals(id) = [];
+                fprintf('Removed %d outliers\n',length(id));
+                
                 T = cat(1,T,dataVals);
             end
+            t = (1:length(T))*0.05/60;
+            Tconv = T*0.112984829; % N-m
+            maxTconv = max(Tconv);
+            minTconv = min(Tconv);
+            avgTconv = mean(abs(Tconv));
+            f = figure;
+            hold on
+            plot(t,Tconv);
+            plot([0 max(t)],[maxTconv maxTconv],'k--');
+            plot([0 max(t)],[minTconv minTconv],'k--');
+            plot([0 max(t)],[avgTconv avgTconv],'k--');
+            title(sprintf('Max=%6.2f N-m; Min=%6.2f N-m; AvgMag=%6.2f N-m', maxTconv, minTconv, avgTconv) )
+            xlabel('Duration, minutes')
+            ylabel('Torque, Nm')
+            f.Name = p;
+            f.Position = [50 50 800 450];
+            drawnow
+
             
-            plot(T);
         end
     end
 end

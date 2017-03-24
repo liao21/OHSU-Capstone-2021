21047beacea426992af716c4b284199f69366726
rarmiger <rarmiger@gmail.com>
2017-03-24 17:39:20 -0400

    updated angle formatting logging and added log parsing

--- +DataAnalysis/ParsePythonData.m
+++ +DataAnalysis/ParsePythonData.m
@@ -73,7 +73,7 @@ classdef ParsePythonData
             end
         end
         
-        function C = get_hci_log(file)
+        function [batt_vals, time_stamp] = get_hci_log(file)
             % DataAnalysis.ParsePythonData.get_hci_log('hci0_myo.log')
             if nargin < 1
                 p = '';
@@ -81,19 +81,232 @@ classdef ParsePythonData
                 %f = 'MPL_WWW_1999-12-31_19-00-25.log';
                 file = fullfile(p,f);
             end
-            %%
+            
+            %% Read file
             filetext = fileread(file);
             
-            %%
-            C = str2double(regexp(filetext,'(?<=Battery Level: )[0-9]+','match'))
+            % Break file into lines
+            lines = strsplit(filetext, '\n');
+            fprintf('Found %d lines\n',length(lines))
             
+            % Find Battery lines            
+            battId = ~cellfun(@(x)isempty(strfind(x,'Battery Level')),lines);
+            battLines = lines(battId);
+            fprintf('Found %d Battery Level lines\n',sum(battId))
+
+            batt_vals = str2double(cellfun(@(x)regexp(x,'(?<=Battery Level: )[0-9]+','match'),battLines));
             
-            %%
+            % extract date string
+            C = cellfun(@(x)regexp(x,'(?<=\]:).*(?=Battery Level)','match'),battLines);
+            % convert to date number
+            time_stamp = datenum(C,'yyyy-mm-dd HH:MM:SS,FFF');
             
-            
+            %% Plot all
+            clf
+            plot(time_stamp,batt_vals,'-')
+            datetick('x','mmm/dd HH:MMPM','keepticks')
+            %set(gca,'XTick',linspace(d2(1),d2(end),20))
+            xtickangle(25)
+            %ylim([18 26])
+            ylabel('Volts')
+            %% Plot by day
+            clf
+            days = unique(round(time_stamp));
+            for i = 1:length(days)
+                subplot(length(days),1,i)
+                id = round(time_stamp - (-0.4)) == days(i);
+                plot(time_stamp(id),batt_vals(id),'-')
+                datetick('x','mmm/dd HH:MMPM','keepticks')
+                %set(gca,'XTick',linspace(d2(1),d2(end),20))
+                xtickangle(25)
+                %ylim([18 26])
+                ylabel('Battery %')
+            end
             
             
         end
+        
+        function get_minivie_log(file)
+            % DataAnalysis.ParsePythonData.get_minivie_log('hci0_myo.log')
+            if nargin < 1
+                p = '';
+                f = 'hci0_myo.log';
+                %f = 'MPL_WWW_1999-12-31_19-00-25.log';
+                file = fullfile(p,f);
+            end
+            
+            %%
+            p = 'C:\tmp\';
+            s = rdir(fullfile(p,'*.log'));
+            % Sort by date
+            [~, id] = sort([s(:).datenum]);
+            s = s(id);
+
+            % Read full file and line splits as single string
+            filetext = [];
+            for i = 1:length(s)
+                f = s(i).name;
+                fprintf('%s\n',f)
+                filetext = cat(2,filetext,fileread(f));
+            end
+            
+            % Break file into lines
+            lines = strsplit(filetext, '\n');
+            fprintf('Found %d lines\n',length(lines))
+            
+            %
+            
+            % Find Battery lines            
+            %battId = ~cellfun(@(x)isempty(strfind(x,'Battery Level')),lines);
+
+            %%
+            
+            % Joint Angle lines
+            % can't do this since no guarantee log messages are in order
+            %angleId = ~cellfun(@(x)isempty(strfind(x,'Joint Command:')),lines);
+            % shift from Joint Command: line to data line: [ ]
+            %angleId = circshift(angleId,1);
+            %dLines = lines(angleId)';
+            
+            
+            angleId = (cellfun(@length,lines) > 200) & (cellfun(@(x)sum(x == ','),lines) >= 27);
+            dLines = lines(angleId)';
+            
+            
+            %% Slow
+            jointAngles = zeros(length(dLines),27);
+            for i = 1:length(dLines)
+                C = regexp(dLines{i},'(?<=INFO).*(?=\])','match');
+                jointAngles(i,:) = str2num(strrep(C{1}(6:end),'''',''));
+                if ~mod(i,1000000)
+                    fprintf('%f %%',i/length(dLines))
+                end
+            end
+
+            
+            %% Plot all Joint Angles
+            plot(jointAngles(:,1:7)*180/pi)
+            legend('SHFE','SHAA', 'HR', 'EL', 'WROT', 'WDEV', 'WFE')
+            ylabel('Joint Angle, deg')
+            
+            figure
+            plot(jointAngles(:,8:27)*180/pi)
+            m = properties(MPL.EnumArm);
+            legend(m(8:end),'Interpreter','None')
+            ylabel('Joint Angle, deg')
+            
+            %% Plot Joint Angles by day
+            clf
+            days = unique(round(time_stamp));
+            for i = 1:length(days)
+                subplot(length(days),1,i)
+                id = round(time_stamp - (-0.4)) == days(i);
+                plot(time_stamp(id),bus_voltage(id),'-')
+                datetick('x','mmm/dd HH:MMPM','keepticks')
+                %set(gca,'XTick',linspace(d2(1),d2(end),20))
+                xtickangle(25)
+                %ylim([18 26])
+                ylabel('Volts')
+            end
+            
+            
+
+            %% Replay in unity
+            a = MPL.MplUnitySink
+            a.initialize()
+            
+            StartStopForm([])
+            i = 0;
+            while StartStopForm
+                drawnow
+                i = i + 1;
+                a.putData(jointAngles(i,:))
+            end
+            
+            
+            
+            
+            
+            
+            %%
+            C = cellfun(@(x)regexp(x,'(?<=bus_voltage'':\s).*(?=,\s''lc_)','match'),bLines);
+            bus_voltage = str2double(C)
+            
+            
+            %%
+            
+            %%%%%%%%%%%%%%%%%%
+            % Parse Hearbeat lines
+            %%%%%%%%%%%%%%%%%%
+            
+            % Find NFU Heartbeat 
+            battId = ~cellfun(@(x)isempty(strfind(x,'bus_voltage')),lines);
+            bLines = lines(battId)';
+            
+            % extract date string
+            C = cellfun(@(x)regexp(x,'.*(?= [NfuUdp)','match'),bLines);
+            % convert to date number
+            time_stamp = datenum(C,'yyyy-mm-dd HH:MM:SS,FFF');
+            
+            % search between "bus_voltage': " and " 'lc"
+            C = cellfun(@(x)regexp(x,'(?<=bus_voltage'':\s).*(?=,\s''lc_)','match'),bLines);
+            bus_voltage = str2double(C);
+            
+            % lc_software_state': '        ', 'nfu_ms_per_CMDDOM
+            C = cellfun(@(x)regexp(x,'(?<=lc_software_state'':\s'').*(?='',\s''nfu_ms_per_CMDDOM)','match'),bLines);
+            lc_software_state = unique(C);
+
+            C = cellfun(@(x)regexp(x,'(?<=nfu_ms_per_CMDDOM'':\s).*(?=,\s''nfu_state)','match'),bLines);
+            nfu_ms_per_CMDDOM = str2double(C);
+            
+            C = cellfun(@(x)regexp(x,'(?<=nfu_state'':\s'').*(?='',\s''nfu_ms_per_ACTUATEMPL)','match'),bLines);
+            nfu_state = unique(C);
+            
+            C = cellfun(@(x)regexp(x,'(?<=nfu_ms_per_ACTUATEMPL'':\s).*(?=,\s''lmc_software_state)','match'),bLines);
+            nfu_ms_per_ACTUATEMPL = str2double(C);
+
+            %% Plot command rate by day
+            clf
+            days = unique(round(time_stamp));
+            for i = 1:length(days)
+                subplot(length(days),1,i)
+                hold on
+                id = round(time_stamp - (-0.4)) == days(i);
+                plot(time_stamp(id),nfu_ms_per_CMDDOM(id),'-')
+                plot(time_stamp(id),nfu_ms_per_ACTUATEMPL(id),'-')
+                datetick('x','mmm/dd HH:MMPM','keepticks')
+                %set(gca,'XTick',linspace(d2(1),d2(end),20))
+                xtickangle(25)
+                %ylim([18 26])
+                h = legend('nfu_ms_per_CMDDOM','nfu_ms_per_ACTUATEMPL');
+                set(h,'Interpreter','None')
+            end
+            
+            %% Plot by day
+            clf
+            days = unique(round(time_stamp));
+            for i = 1:length(days)
+                subplot(length(days),1,i)
+                id = round(time_stamp - (-0.4)) == days(i);
+                plot(time_stamp(id),bus_voltage(id),'-')
+                datetick('x','mmm/dd HH:MMPM','keepticks')
+                %set(gca,'XTick',linspace(d2(1),d2(end),20))
+                xtickangle(25)
+                %ylim([18 26])
+                ylabel('Volts')
+            end
+            
+            
+            
+            
+            %% Get Battery Info:
+            
+            
+            
+            
+            
+            
+        end            
         
         
         

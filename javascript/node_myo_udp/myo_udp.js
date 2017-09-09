13665fa8eaf2fcc32011f88974692b8a59002de3
rarmiger <rarmiger@gmail.com>
2017-09-09 15:25:27 -0400

    formatting update

--- javascript/node_myo_udp/myo_udp.js
+++ javascript/node_myo_udp/myo_udp.js
@@ -14,39 +14,39 @@ for(var i = 0; i<process.argv.length; i++){
 node myo_udp --n(optional) numBands --option1 value1 value2 ... --option2 value1 value2 ...
 
 OPTIONS:
---n 	number of armbands.
+--n     Number of armbands.
 
-	Default 1 armband. Takes one value determining number of armbands. If used, must be first option.
+    Default 1 armband. Takes one value determining number of armbands. If used, must be first option.
 
---ADD 	MAAC address.
-	 Default for 1 armband can be set above. Value is armband maac address without ':' characters. Can include as many as necessary.
+--ADD   MAAC address.
+    Default for 1 armband can be set above. Value is armband maac address without ':' characters. Can include as many as necessary.
 
---PORT	Destination port to send to.
-		Default is 15001. Include values as necessary in same order as maac address.
-		
---IP 	Destination IP.
-		Default is localhost. Include values as necessary in same order as maac address.
+--PORT  Destination port to send to.
+    Default is 15001. Include values as necessary in same order as maac address.
+
+--IP    Destination IP.
+    Default is localhost. Include values as necessary in same order as maac address.
 */
-	if(process.argv[i] == "--ADD"){ //maac addresses
-		for(var j = 1; j<=numBands;j++){
-			maacAddress[j-1] = process.argv[i+j];
-		}	}
-	else if(process.argv[i] == "--PORT"){ //ports
-		for(var k = 1; k<=numBands;k++){
-			port[k-1] = process.argv[i+k];
-		}
-	}
-	else if(process.argv[i] == "--IP"){ //ip addresses
-		for(var m = 1; m<=numBands;m++){
-			ipAdd[m-1] = process.argv[i+m];
-		}	
-	}
-	else if(process.argv[i] == "--n"){ //number of armbands
-		numBands = process.argv[i+1];
-	}
-	else if(process.argv[i] == "--DEBUG"){
-		debug = process.argv[i+1];
-	}
+    if(process.argv[i] == "--ADD"){ //maac addresses
+        for(var j = 1; j<=numBands;j++){
+            maacAddress[j-1] = process.argv[i+j];
+        }   }
+    else if(process.argv[i] == "--PORT"){ //ports
+        for(var k = 1; k<=numBands;k++){
+            port[k-1] = process.argv[i+k];
+        }
+    }
+    else if(process.argv[i] == "--IP"){ //ip addresses
+        for(var m = 1; m<=numBands;m++){
+            ipAdd[m-1] = process.argv[i+m];
+        }
+    }
+    else if(process.argv[i] == "--n"){ //number of armbands
+        numBands = process.argv[i+1];
+    }
+    else if(process.argv[i] == "--DEBUG"){
+        debug = process.argv[i+1];
+    }
 }
 
 MyoAgent.setAddress(maacAddress);
@@ -59,49 +59,42 @@ console.log("Agent MAAC address:" + MyoAgent.MAACaddress);
 console.log("Port:" + MyoAgent.port);
 console.log("IP Addresses:" + MyoAgent.ipAdd);
 if(debug > 0){
-	console.log ("Debug Level:" + debug);
+    console.log ("Debug Level:" + debug);
 }
 
 /*************************************************************/
 MyoAgent.on('discovered', function(armband){
-	armband.on('connect', function(connected){
-    
-    	// armband connected succesfully
+    armband.on('connect', function(connected){
+
+        // armband connected succesfully
         if(connected){
             // discover all services/characteristics and enable emg/imu/classifier chars
-        	this.initStart();
-    	} else {
-    	  // armband disconnected
-   		}
-
-	});
+            this.initStart();
+        } else {
+            // armband disconnected
+        }
+    });
     
     // Armband receives the ready event when all services/characteristics are discovered and emg/imu/classifier mode is enabled
     armband.on('ready', function(){
-   
-
-    	// register for events
+        // register for events
         armband.on('batteryInfo',function(data){
-        	if(debug == 2){
-				console.log('BatteryInfo: ', data.batteryLevel); //only for critical debug mode
-			}
-		
-		});
-        
+            if(debug == 2){
+                console.log('BatteryInfo: ', data.batteryLevel); //only for critical debug mode
+            }
+        });
+
         // read or write data from/to the Myo
         armband.readBatteryInfo();
-		armband.setMode();
-		armband.on('emg', function(data1){
-			//Comment the next line out to remove logging of EMG data
-			if(debug == 2){
-				console.log("EMG DATA: " + data1.emgData.sample2); //only for critical debug mode
-			}
-			process.stdout.clearLine();
-			process.stdout.cursorTo(0);
-		});	
+        armband.setMode();
+        armband.on('emg', function(data1){
+            //Comment the next line out to remove logging of EMG data
+            if(debug == 2){
+                console.log("EMG DATA: " + data1.emgData.sample2); //only for critical debug mode
+            }
+            process.stdout.clearLine();
+            process.stdout.cursorTo(0);
+        }); 
     });
-	
-   
-    
-   armband.connect();
+    armband.connect();
 });

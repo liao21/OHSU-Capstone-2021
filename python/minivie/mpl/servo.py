93917bdd41b97877b816e022c0fbb1d6ed67b7df
Ryan Regier <regier21@up.edu>
2021-04-19 21:38:27 -0700

    Fixed typo

--- python/minivie/mpl/servo.py
+++ python/minivie/mpl/servo.py
@@ -246,7 +246,7 @@ class Servo(DataSink):
             esp2[1] += wrist_rot
         
         esp2[0] = max(wrist_fe, esp2[0])
-        esp2[1] += max(wrist_fe, esp2[1])
+        esp2[1] = max(wrist_fe, esp2[1])
         esp2[2] = thumb_ab_ad
 
         # Send data
